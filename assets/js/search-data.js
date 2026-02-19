ba9bf7c8af31dd6a0374a249ae4564ef43835dbf
yuxzhou <73013705+yuxzhou@users.noreply.github.com>
2026-02-19 14:22:20 +0000

    Deploying to gh-pages from @ yuxzhou/yuxzhou.github.io@543acdecf5d2cb1d41ec6b0a5beedf281a1ba9e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%75%78%69%6E%67.%7A%68%6F%75@%63%68%65%6D.%6F%78.%61%63.%75%6B", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,7 +539,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=e2bnb2cAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/yuxing-zhou-400581200", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/yuxzhou", "_blank");
         },
       },{
         id: 'social-custom_social',
